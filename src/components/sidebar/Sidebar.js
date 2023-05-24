49661b2af09d0b2bca76ecb1364cd2febe29438a
YuyaIdo <y.ido14h@gmail.com>
2023-05-24 14:29:08 +0900

    first commit

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* ツイッターアイコン */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* ツイートボタン */}
+      サイドバー
+    </div>
+  );
+}
+
+export default Sidebar;
